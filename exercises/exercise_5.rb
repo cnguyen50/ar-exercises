a8f5753413dbd39d0eae3adb0f467172effd0ca6
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-03-31 01:48:55 +0000

    Calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,16 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.pluck(:annual_revenue).sum
+@average_revenue = @total_revenue/Store.count
+@million_revenue = Store.where("annual_revenue > ?", 1000000).count
+
+puts "Total revenue:"
+pp @total_revenue
+
+puts "Average revenue:"
+pp @average_revenue
+
+puts "$1M or more in annual sales"
+pp @million_revenue
