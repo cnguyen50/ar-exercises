4276143b2d3f4d07e9e7a29055673816fd593d6e
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-03-31 01:56:13 +0000

    One to many and Inserted Employee data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,20 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create([
+  {first_name: "Khurram", last_name: "Virani", hourly_rate: 60},
+  {first_name: "Calvin", last_name: "Nguyen", hourly_rate: 40}
+])
+
+
+@store2.employees.create([
+  {first_name: "Will", last_name: "Smith", hourly_rate: 80},
+  {first_name: "Mary", last_name: "Poppins", hourly_rate: 90}
+])
+
+puts "-----Store 1 employees ------"
+pp @store1.employees
+
+puts "----- Store 2 employees ------"
+pp @store2.employees
