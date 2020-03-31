c79d80d696baff006a0f55346731bc47aef8cc78
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-03-31 01:40:41 +0000

    loading subset of stores with 2 fetches

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -13,3 +13,17 @@ stores = Store.create([
   {:name => 'Whistler', :annual_revenue => 1900000, :mens_apparel => 'true', :womens_apparel => 'false'},
   {:name => 'Yaletown', :annual_revenue => 430000, :mens_apparel => 'true', :womens_apparel => 'true'}
 ])
+
+@men_stores = Store.where(mens_apparel: true)
+puts "Stores with men apparel"
+pp @men_stores
+
+puts "Their names and annual revenue:"
+@men_stores.each do |store|
+  puts "name: #{store.name}"
+  puts "annual revenue: #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = ? and annual_revenue < ?", true, 1000000)
+puts "stores carrying womens apparel and having annual revenue less than 1000000"
+pp @womens_stores
